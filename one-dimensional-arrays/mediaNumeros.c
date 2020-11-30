477cf3949d8e86aea17e3493dd4dd406f0b02b26
Mauricio Massaru Arimoto <mauricioarimoto@gmail.com>
2020-11-29 23:22:14 -0300

    Update mediaNumeros.c

--- one-dimensional-arrays/mediaNumeros.c
+++ one-dimensional-arrays/mediaNumeros.c
@@ -1,25 +1,34 @@
-/* TheHuxley - Problema Ordenacao ingenua*/
+/* TheHuxley - Problema Media de n numeros*/
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-  int n;
-  scanf("%d", &n);
-  int i, j,aux;
-  int vetor[n];
+  int i, quant=0;
+  float count=0, total=0, var=0, media=0;
+  float vetor[10000];
 
-  for (i=0; i<n; i++) 
-    scanf("%d", &vetor[i]);
-  
-  for(i=0; i<n; i++) {
-    for (j=i+1; j<n; j++) {
-      if(vetor[i] > vetor[j]) {
-        aux = vetor[i];
-        vetor[i] = vetor[j];
-        vetor[j] = aux;
-      }
-    }
+  for (i=0; ; ++i) {
+    scanf("%f",&vetor[i]);
+    if(vetor[i]== -1) 
+      break;
+    quant++;
   }
-  for(i=0; i<n; i++)
-    printf("[%d]", vetor[i]);
+
+  for (i=0 ; i < quant ;++i) //somatoria
+    total += vetor[i];
+  media = total / quant;
+
+  for(i=0 ; i < quant ;++i) //somatoria
+    var += pow((vetor[i] - media), 2);
+
+  var /= quant-1;
+  for(i=0; i < quant;++i) { //Maior que media
+    if(vetor[i] > media) 
+        ++count;
+  }
+
+  printf("%.2f\n", media);
+  printf("%.2f\n", sqrt(var));
+  printf("%.0f\n", count, quant);
   return 0;
 }
